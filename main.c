cd5fa5d4bb50f5759510dd5b9be1485777077633
ptrWilkoo <154998368+ptrWilkoo@users.noreply.github.com>
2024-02-28 20:01:37 +0100

    Create main.c
    Pierwsza wersja pliku. Funkcjonalność: określanie liczby znaków w wierszu, co pozwoli określić wymiar labiryntu.

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Nie podano nazwy pliku!");
+		return 1;
+	}
+
+	FILE *plik;
+	char linia[2050];
+	int liczba_znaków = -1;
+
+	plik = fopen(argv[1], "r");
+
+	if(plik == NULL) {
+		printf("Nie udało się otworzyć pliku!");
+		return 1;
+	}
+
+	if(fgets(linia, sizeof(linia), plik)) {
+		for(int i = 0; linia[i] != '\0'; i++) {
+			liczba_znaków++;
+		}
+	}
+
+	printf("Liczba znaków w wierszu: %d\n", liczba_znaków);
+
+	return 0;
+}
